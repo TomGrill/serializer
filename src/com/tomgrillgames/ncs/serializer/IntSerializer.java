018f3e0c2359eb329257a6154da23db3ffcfe2d2
Thomas Pronold <mail@tomgrill.de>
2017-07-22 19:17:26 +0200

    Add IntSerializer

---
+++ src/com/tomgrillgames/ncs/serializer/IntSerializer.java
@@ -0,0 +1,26 @@
+package com.tomgrillgames.ncs.serializer;
+
+public class IntSerializer {
+
+    public static byte[] serialize(int value) {
+        return new byte[]{
+                (byte) (value >> 24),
+                (byte) (value >> 16),
+                (byte) (value >> 8),
+                (byte) (value)
+        };
+    }
+
+    public static byte[] serialize(byte[] result, int value) {
+        result[0] = (byte) (value >> 24);
+        result[1] = (byte) (value >> 16);
+        result[2] = (byte) (value >> 8);
+        result[3] = (byte) (value);
+        return result;
+    }
+
+    public static int deserialize(byte[] bytes) {
+        return ((bytes[0] & 0xFF) << 24) | ((bytes[1] & 0xFF) << 16) | ((bytes[2] & 0xFF) << 8) | (bytes[3] & 0xFF);
+    }
+
+}
---
+++ src/com/tomgrillgames/ncs/serializer/IntSerializerTests.java
@@ -0,0 +1,26 @@
+package com.tomgrillgames.ncs.serializer;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IntSerializerTests {
+
+    @Test
+    public void serializeDeserializeTest() {
+        assertEquals(Integer.MAX_VALUE, IntSerializer.deserialize(IntSerializer.serialize(Integer.MAX_VALUE)));
+        assertEquals(Integer.MIN_VALUE, IntSerializer.deserialize(IntSerializer.serialize(Integer.MIN_VALUE)));
+        assertEquals(0, IntSerializer.deserialize(IntSerializer.serialize(0)));
+        assertEquals(41230, IntSerializer.deserialize(IntSerializer.serialize(41230)));
+        assertEquals(-41230, IntSerializer.deserialize(IntSerializer.serialize(-41230)));
+
+    }
+
+    @Test
+    public void serializeDeserializeWithResultTest() {
+        byte[] result = new byte[4];
+        IntSerializer.serialize(result, Integer.MAX_VALUE);
+        assertEquals(Integer.MAX_VALUE, IntSerializer.deserialize(result));
+    }
+}
